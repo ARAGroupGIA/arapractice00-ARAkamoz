2c88918d416d9bc92c2b4d09598dfb2bc325ae41
ARAkamoz <55489431+ARAkamoz@users.noreply.github.com>
2019-09-18 13:28:46 +0200

    My First Program

--- Exercise1/exercise1.c
+++ Exercise1/exercise1.c
@@ -1,9 +1,15 @@
 /******************* Problem definition **********************************/
 /* Implement a program that it shows the message "Hello World" on screen. */
 /*************************************************************************/
-
+#include <stdio.h>
 /* Here, you must include the required libraries */
 
 void main(){
+	printf("Hello World Version 1");
 	/* Here, you must write the source code to show the missatge "Hello world" on screen*/
 }
+
+
+
+
+
