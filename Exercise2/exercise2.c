34fe97b297f80e0caa135d59bc31cbf2162382b1
ARAkamoz <55489431+ARAkamoz@users.noreply.github.com>
2019-09-18 14:04:56 +0200

    version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,14 @@
 /* Here, you should entry the required libraries */
-
+#include <stdio.h>
 void main(){
 	/* Here, you should write the source code of your program */
+
+int a=2,b=3, S;
+
+ scanf("%d %d",&a,&b);
+ S= a + b;
+ 
+ printf("The Sum is %d .\n",S);
+ 
+ 
 }
